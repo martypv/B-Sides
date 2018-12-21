b6afdcc11749f9ce01968455f1e3505647bcd5bf
Martin <Martin@Martins-MBP.lan1>
2018-12-20 22:47:10 -0500

    Error Pages

--- Drunk Playlist.py
+++ Drunk Playlist.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.errorhandler(404)
+def notfound(e):
+    return render_template('error404.html'), 404
+
+
+@app.errorhandler(500)
+def servererror(e):
+    return render_template('error500.html'), 500
 
 
 if __name__ == '__main__':
