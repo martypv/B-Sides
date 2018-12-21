d80c33c5d25cdd8689c087ab3ed90004916dc9a3
Martin <Martin@Martins-MBP.lan1>
2018-12-21 14:50:18 -0500

    Flask Form Practice

--- Drunk Playlist.py
+++ Drunk Playlist.py
@@ -1,9 +1,20 @@
+import os,random,string
+from flask import Flask, render_template, session, redirect, url_for
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, IntegerField, PasswordField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'pleasedont'
 
 bootstrap = Bootstrap(app)
+
+class joinForm(FlaskForm):
+    code = StringField('Room Code: ', validators=[DataRequired()])
+    submit = SubmitField('OK')
 
 
 @app.route('/')
@@ -19,6 +30,18 @@ def notfound(e):
 @app.errorhandler(500)
 def servererror(e):
     return render_template('error500.html'), 500
+
+@app.route('/frame')
+def frame():
+    return render_template('frame.html')
+
+@app.route('/join', methods=['GET', 'POST'])
+def joinRoom():
+    form = joinForm()
+    if form.validate_on_submit():
+        session['code'] = form.code.data
+        return redirect(url_for('joinRoom'))
+    return render_template('join.html', form=form, code=session.get('code'))
 
 
 if __name__ == '__main__':
